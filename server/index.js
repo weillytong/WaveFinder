1c3635f55516c0f033f02853b3c4d61a421367a1
weillytong <weillydtong@gmail.com>
2021-03-29 20:08:02 -0700

    downloaded webpack and setup front-end to test

--- server/index.js
+++ server/index.js
@@ -16,6 +16,10 @@ server.use(cors());
 server.use(bodyparser.json());
 server.use(bodyparser.urlencoded({ extended: true }));
 
+// Serve up static files
+server.use(express.static(path.join(__dirname, '../client/dist')));
+
+// Connect to router
 server.use('/api', router);
 
 server.listen(port, () => console.log(`LISTENING ON PORT ${port}`));
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+// const BrotliPlugin = require('brotli-webpack-plugin');
+// const CompressionPlugin = require('compression-webpack-plugin');
+
+module.exports = {
+  watch: true,
+  mode: 'production',
+  entry: path.resolve(__dirname, './client/src'),
+  output: {
+    path: path.resolve(__dirname, './client/dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        loader: 'babel-loader',
+        test: /\.js[x]?/,
+        exclude: /node_modules/,
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    // new BrotliPlugin({ //brotli plugin
+    //   asset: '[path].br[query]',
+    //   test: /\.(js|css|html|svg)$/,
+    //   threshold: 10240,
+    //   minRatio: 0.8
+    // })
+  //   new CompressionPlugin({
+  //   filename: '[path].gz[query]',
+  //   algorithm: 'gzip',
+  //   test: /\.(js|css|html|svg)$/,
+  //   threshold: 8192,
+  //   minRatio: 0.8
+  //   })
+  ],
+};
