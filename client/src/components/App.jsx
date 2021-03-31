a2f3709f5f4bc083012649ebb1f91a305b712e33
weillytong <weillydtong@gmail.com>
2021-03-31 12:20:26 -0700

    Added some CSS and fonts to homepage

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import Select from 'react-select';
 import BeachList from './BeachList.jsx';
+import HomePage from './HomePage.jsx';
 
 const options = [
   {value: 'All Cities', label: 'All Cities'},
@@ -16,7 +17,7 @@ export default class App extends React.Component {
     this.state = {
       beachList: [],
       selectedBeachList: [],
-      city: ''
+      city: 'none'
     };
     this.getReport = this.getReport.bind(this);
     this.handleChange = this.handleChange.bind(this);
@@ -53,52 +54,31 @@ export default class App extends React.Component {
   }
 
   render() {
-    if (this.state.city === '') {
-      return (
-        <div>
+    let list;
+    if (this.state.city === 'none') {
+      list = <HomePage />
+    } else if (this.state.city === 'All Cities') {
+      list = <BeachList beachList={this.state.beachList}/>
+    } else {
+      list = <BeachList beachList={this.state.selectedBeachList}/>
+    }
+    return (
+      <div>
         <div className="header">
           <img className="logo" src="/images/wave.svg"/> Wave Finder
-        </div>
-        </div>
-      )
-    } else if (this.state.city === 'All Cities') {
-      return (
-        <div>
-            <div className="header">
-              <img className="logo" src="/images/wave.svg"/> Wave Finder
-            </div>
-            <div className="navigation">
-              {/* <Select options={options}/> */}
-              <select value={this.state.city} onChange={this.handleChange}>
-                <option> Select an Option </option>
-                <option value="All Cities"> All Cities </option>
-                <option value="San Diego"> San Diego </option>
-                <option value="Los Angeles"> Los Angeles </option>
-              </select>
-            </div>
-          {/* <iframe name="goog_1212536477" sandbox="allow-scripts allow-same-origin" srcdoc="<script src=https://pagead2.googlesyndication.com/omsdk/releases/live/omweb-v1.js></script>" ></iframe> */}
-          <BeachList beachList={this.state.beachList}/>
-        </div>
-      )
-    } else {
-      return (
-        <div>
-            <div className="header">
-              <img className="logo" src="/images/wave.svg"/> Wave Finder
-            </div>
           <div className="navigation">
             {/* <Select options={options}/> */}
-            <select value={this.state.city} onChange={this.handleChange}>
-              <option> Select an Option </option>
+            <select className="dropdown" value={this.state.city} onChange={this.handleChange}>
+              <option value='none'> -- Select an Location -- </option>
               <option value="All Cities"> All Cities </option>
               <option value="San Diego"> San Diego </option>
               <option value="Los Angeles"> Los Angeles </option>
             </select>
           </div>
-          {/* <iframe name="goog_1212536477" sandbox="allow-scripts allow-same-origin" srcdoc="<script src=https://pagead2.googlesyndication.com/omsdk/releases/live/omweb-v1.js></script>" ></iframe> */}
-          <BeachList beachList={this.state.selectedBeachList}/>
         </div>
-      )
-    }
+        {/* <iframe name="goog_1212536477" sandbox="allow-scripts allow-same-origin" srcdoc="<script src=https://pagead2.googlesyndication.com/omsdk/releases/live/omweb-v1.js></script>" ></iframe> */}
+        {list}
+      </div>
+    )
   }
 }
---
+++ client/src/components/HomePage.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+export default class HomePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="homePageTitle">
+          Find Your Perfect Wave
+        </div>
+      </div>
+    )
+  }
+}
