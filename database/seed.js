4fb43652f03db3e72fb4f43b08be7c2256626d8d
weillytong <weillydtong@gmail.com>
2021-03-31 14:56:23 -0700

    Reseeded with new metrics

--- database/seed.js
+++ database/seed.js
@@ -6,13 +6,13 @@ Surf.insertMany([
     beach: 'La Jolla Shores',
     beachImg: 'http://joeropersurfboardrepair.com/wp-content/uploads/2016/04/Screen-Shot-2016-04-12-at-7.24.45-PM.png',
     city: 'San Diego',
-    surf: '1-2',
+    surf: '1 - 2',
     report: 'Still on the smaller side this afternoon as primary SSW swell rolls in over smaller NW swell mix, good for mostly knee-waist high surf. Some texture from light+ to locally moderate westerly onshore wind but overall it\'s pretty manageable with soft, cruisy corners to work with. Tide is still on the full side, dropping down to a 4:20pm low of -0.1.',
     tide: '1.8',
     wind: '5',
     swell: '1.6 ft at 16s SW 221',
     water: '57 - 58',
-    weather: '53',
+    weather: '58',
     suit: [
       {
         img: 'https://images.boardriders.com/global/billabong-products/all/default/hi-res/mwfu3bf4_billabong,v_blk_sd1.jpg',
@@ -27,18 +27,18 @@ Surf.insertMany([
     beach: 'Pacific Beach',
     beachImg: 'https://upload.wikimedia.org/wikipedia/commons/7/70/Pacific_Beach_Skyline.jpg',
     city: 'San Diego',
-    surf: '2-3',
-    report: 'Still on the smaller side this afternoon as primary SSW swell rolls in over smaller NW swell mix, good for mostly knee-waist high surf. Some texture from light+ to locally moderate westerly onshore wind but overall it\'s pretty manageable with soft, cruisy corners to work with. Tide is still on the full side, dropping down to a 4:20pm low of -0.1.',
-    tide: '1.8',
+    surf: '3 - 4',
+    report: ' Basically clean conditions going into the afternoon with just a slight sea breeze. Long period SSW swell slowly fills in as NW swell mix eases, and the tide tops out at a 3.9 high at 12:00pm. Somewhat slowed surf is in the waist to chest high zone at good spots. ',
+    tide: '2.5',
     wind: '6',
-    swell: '1.7 ft at 15s SW 215',
-    water: '57 - 58',
-    weather: '53',
+    swell: '1.8 ft at 17s SW 217',
+    water: '60 - 61',
+    weather: '58',
     suit: [
       {
-        img: 'https://images.boardriders.com/global/billabong-products/all/default/hi-res/mwfu3bf4_billabong,v_blk_sd1.jpg',
-        suitName: '4/3 Furnace Carbon Chest Zip',
-        website: 'https://www.billabong.com/4/3-furnace-carbon-chest-zip-MWFUQBF4.html'
+        img: 'https://images.boardriders.com/global/billabong-products/all/default/hi-res/mwfu3bc3_billabong,v_blk_sd1.jpg',
+        suitName: '3/2 Absolute Chest Zip Wetsuit',
+        website: 'https://www.billabong.com/3/2-absolute-chest-zip-wetsuit-194843011863.html#cgid=men_surf_wetsuits&prefn1=billabong-thickness&prefv1=3%2F2%20mm&start=6&hitcount=6'
       }
     ],
     livestream: 'https://www.surfline.com/surf-report/pacific-beach/5842041f4e65fad6a7708841'
@@ -48,18 +48,18 @@ Surf.insertMany([
     beach: 'Old Man\'s at Tourmaline',
     beachImg: 'https://www.californiabeaches.com/wp-content/uploads/2014/09/Tourmaline-Surf-Park-Beach-Bryce-Apr2016-4-1000x532.jpg',
     city: 'San Diego',
-    surf: '1-2',
+    surf: '1 - 2',
     report: 'Still on the smaller side this afternoon as primary SSW swell rolls in over smaller NW swell mix, good for mostly knee-waist high surf. Some texture from light+ to locally moderate westerly onshore wind but overall it\'s pretty manageable with soft, cruisy corners to work with. Tide is still on the full side, dropping down to a 4:20pm low of -0.1.',
     tide: '1.8',
     wind: '6',
     swell: '1.7 ft at 15s SW 215',
-    water: '57 - 58',
-    weather: '57',
+    water: '60 - 61',
+    weather: '58',
     suit: [
       {
-        img: 'https://images.boardriders.com/global/billabong-products/all/default/hi-res/mwfu3bf4_billabong,v_blk_sd1.jpg',
-        suitName: '4/3 Furnace Carbon Chest Zip',
-        website: 'https://www.billabong.com/4/3-furnace-carbon-chest-zip-MWFUQBF4.html'
+        img: 'https://images.boardriders.com/global/billabong-products/all/default/hi-res/mwfu3bc3_billabong,v_blk_sd1.jpg',
+        suitName: '3/2 Absolute Chest Zip Wetsuit',
+        website: 'https://www.billabong.com/3/2-absolute-chest-zip-wetsuit-194843011863.html#cgid=men_surf_wetsuits&prefn1=billabong-thickness&prefv1=3%2F2%20mm&start=6&hitcount=6'
       }
     ],
     livestream: 'https://www.surfline.com/surf-report/old-man-s-at-tourmaline/5842041f4e65fad6a77088c4'
@@ -69,13 +69,13 @@ Surf.insertMany([
     beach: 'Black\'s Beach',
     beachImg: 'https://d14fqx6aetz9ka.cloudfront.net/wp-content/uploads/2018/05/03094559/05-1560x866.jpg',
     city: 'San Diego',
-    surf: '2-3',
+    surf: '2 - 3',
     report: 'Still on the smaller side this afternoon as primary SSW swell rolls in over smaller NW swell mix, good for mostly knee-waist high surf. Some texture from light+ to locally moderate westerly onshore wind but overall it\'s pretty manageable with soft, cruisy corners to work with. Tide is still on the full side, dropping down to a 4:20pm low of -0.1.',
     tide: '1.8',
     wind: '5',
     swell: '1.6 ft at 16s SW 221',
     water: '57 - 58',
-    weather: '53',
+    weather: '58',
     suit: [
       {
         img: 'https://images.boardriders.com/global/billabong-products/all/default/hi-res/mwfu3bf4_billabong,v_blk_sd1.jpg',
@@ -90,13 +90,13 @@ Surf.insertMany([
     beach: 'Cardiff Beach',
     beachImg: 'https://www.visitcalifornia.com/sites/visitcalifornia.com/files/styles/welcome_image/public/VC_CoastalCamping_Module1_SanElijo_RF_520618474_1280x640.jpg',
     city: 'San Diego',
-    surf: '2-3',
+    surf: '2 - 3',
     report: 'Still on the smaller side this afternoon as primary SSW swell rolls in over smaller NW swell mix, good for mostly knee-waist high surf. Some texture from light+ to locally moderate westerly onshore wind but overall it\'s pretty manageable with soft, cruisy corners to work with. Tide is still on the full side, dropping down to a 4:20pm low of -0.1',
     tide: '1.9',
     wind: '5',
     swell: '2.3 ft at 16s SW 221',
     water: '57 - 58',
-    weather: '53',
+    weather: '58',
     suit: [
       {
         img: 'https://images.boardriders.com/global/billabong-products/all/default/hi-res/mwfu3bf4_billabong,v_blk_sd1.jpg',
@@ -111,7 +111,7 @@ Surf.insertMany([
     beach: 'Venice Breakwater',
     beachImg: 'https://www.californiabeaches.com/wp-content/uploads/2014/09/bigs-Venice-Beach-Aerial-view-of-the-shoreline-in-Venice-CA-at-dusk-Large-1000x667.jpg',
     city: 'Los Angeles',
-    surf: '2-3',
+    surf: '2 - 3',
     report: 'Even with the 4.6 high tide at 11:30am, overall conditions should stay relatively fun as only a light southerly wind develops through late morning and into the lunch hour. Wind does trend more W/onshore by afternoon but all in all, staying fairly light and manageable. However, surf will be coming down slightly so maybe best to pack some extra board in the car, if thinking about a paddle later in the day.',
     tide: '5.6',
     wind: '3',
@@ -132,18 +132,18 @@ Surf.insertMany([
     beach: 'El Porto Manhattan Beach',
     beachImg: 'https://d1y7kerpdff7pl.cloudfront.net/El%20Porto%20Beach%20Santa%20Monica.jpg',
     city: 'Los Angeles',
-    surf: '3-4',
-    report: 'Looking fun this morning with peaky surf and clean conditions. NW swell is up a peg, while longer-period SSW swell lingers for mostly waist-shoulder high sets. Tide push through the next few hours helps with shape and consistency.',
-    tide: '2.9',
-    wind: '4',
+    surf: '3 - 4',
+    report: ' More of a long-period SSW swell mix is moving in, as shorter-period NW swell eases, winds are light onshore and the tide topped out at a 3.9 high at 12:30pm. End result at report time is mainly textured, slowed looking surf in the thigh-waist-stomach high zone at good spots, while top exposures/combo breaks in the northern Manhattan area see better sets.',
+    tide: '3',
+    wind: '5',
     swell: '2.3 ft at 16s SW 221',
-    water: '57 - 58',
+    water: '60',
     weather: '52',
     suit: [
       {
-        img: 'https://images.boardriders.com/global/billabong-products/all/default/hi-res/mwfu3bf4_billabong,v_blk_sd1.jpg',
-        suitName: '4/3 Furnace Carbon Chest Zip',
-        website: 'https://www.billabong.com/4/3-furnace-carbon-chest-zip-MWFUQBF4.html'
+        img: 'https://images.boardriders.com/global/billabong-products/all/default/hi-res/mwfu3bc3_billabong,v_blk_sd1.jpg',
+        suitName: '3/2 Absolute Chest Zip Wetsuit',
+        website: 'https://www.billabong.com/3/2-absolute-chest-zip-wetsuit-194843011863.html#cgid=men_surf_wetsuits&prefn1=billabong-thickness&prefv1=3%2F2%20mm&start=6&hitcount=6'
       }
     ],
     livestream: 'https://www.surfline.com/surf-report/el-porto-overview/584204214e65fad6a7709d24'
